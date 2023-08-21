6c387a0ae6c63a99b3dbcb2c1592874f7dbc20d3
Celio Maroja <c3rd.dev@gmail.com>
2023-08-21 10:46:52 -0300

    spawning object when interact
    Signed-off-by: Celio Maroja <c3rd.dev@gmail.com>

--- Assets/Scripts/ClearCounter.cs
+++ Assets/Scripts/ClearCounter.cs
@@ -4,8 +4,15 @@ using UnityEngine;
 
 public class ClearCounter : MonoBehaviour
 {
+
+    [SerializeField] private Transform tomatoPrefab;
+    [SerializeField] private Transform counterTopPoint;
+
+
     public void Interact()
     {
         Debug.Log("Interacted");
+        Transform tomatoTransform = Instantiate(tomatoPrefab, counterTopPoint);
+        tomatoTransform.localPosition = Vector3.zero;
     }
 }
